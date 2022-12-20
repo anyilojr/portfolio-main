9bcea5854ed26a135de3ad7ecbd1d85781c4d8f7
Anyilo <anyilojoserangelarias@gmail.com>
2022-12-20 17:42:10 -0500

    Segundo intento

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css|.styl$/i,
+            use: [MiniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ],
+        },
+        {
+            test: /\.png/, 
+            type: 'asset/resource'
+        },
+        {
+            test:/\.(woff|woff2)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'assets/fonts/[hash][ext][query]',
+            },
+        },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+    ]
+}
